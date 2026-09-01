0acc4e75caab79865b37012e04cc005a975836dd
cnisland <77559538+cnisland@users.noreply.github.com>
2026-09-01 14:13:16 +0800

    fix: drizzle logger string "false" causes logQuery error

--- mail-worker/src/entity/orm.js
+++ mail-worker/src/entity/orm.js
@@ -1,5 +1,6 @@
 import { drizzle } from 'drizzle-orm/d1';
 
 export default function orm(c) {
-	return drizzle(c.env.db,{logger: c.env.orm_log})
+	const logger = c.env.orm_log === true || c.env.orm_log === 'true';
+	return drizzle(c.env.db, { logger })
 }
